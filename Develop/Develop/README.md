e0156c8179cc82984e5972694855401efd9df71d
Judith Nwagwu <Judith5nwagwu@gmail.com>
2020-09-01 19:18:58 -0700

    adding gif to ReadMe

--- Develop/Develop/README.md
+++ Develop/Develop/README.md
@@ -1,2 +1,35 @@
-# judy
+#Title
+   Judy
+  ## Description
+  app
+  ## Badge
+  undefined
+  ## Table Of Contents
+ 
+  ## Installation
+  install npm
+
+  ## Usage
+  2222
+
+  ## License
+  1234
+
+  ## Contributors
+
+  Judy
+
+  ## Tests
+
+  123
+
+  ## Repository
+
+  link
+
+  ## Github
+
+  null
+  https://github.com/judithn226
+
 
--- Develop/Develop/utils/generateMarkdown.js
+++ Develop/Develop/utils/generateMarkdown.js
@@ -1,6 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data, repoInfo) {
-  return `# ${data.title}
+  return `#Title
+   ${data.title}
+  ## Description
+  ${data.description}
+  ## Badge
+  ${data.badge}
+  ## Table Of Contents
+ 
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.use}
+
+  ## License
+  ${data.license}
+
+  ## Contributors
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Repository
+
+  ${data.repo}
+
+  ## Github
+
+  ${repoInfo.name}
+  ${repoInfo.profile}
+
 
 `;
 }
